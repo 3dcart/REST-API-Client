f3880134acfc7898e19020411a7d3901197f9637
nkhalid <nkhalid@3dcartpc095.3dc.local>
2016-02-05 16:00:17 -0500

    Rest API Client - C#

--- DCartAPIClient/Scripts/filters.js
+++
@@ -1,9 +0,0 @@
-﻿'use strict';
-
-angular.module('app.filters', [])
-
-    .filter('interpolate', ['version', function (version) {
-        return function (text) {
-            return String(text).replace(/\%VERSION\%/mg, version);
-        }
-    }]);
